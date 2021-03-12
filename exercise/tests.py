16dbf3374a9c8eefca08704d8727c2a294b0d858
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2021-03-12 18:36:11 -0500

    Updating test case naming scheme

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
-class randomTestCase(TestCase):
-    def sample_test_case(self):
+class Test(TestCase):
+    def test_case(self):
+        """
+        Sample test to see if the url 404s or not
+        """
         response = self.client.get(url)
         error404 = False
         if(response.status_code == 404):
