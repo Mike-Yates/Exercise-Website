03130a5f1aa8cc67a86fe6a0fb2fb39d8da8e4f7
Nathaniel Gonzalez <neg2mhs@virginia.edu>
2021-03-12 18:28:21 -0500

    Update tests.py
    added a test case that checks if the site 404 errors

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class randomTestCase(TestCase):
+    def sample_test_case(self):
+        response = self.client.get(url)
+        error404 = False
+        if(response.status_code == 404):
+            error404 = True
+        self.assertIs(error404, False)
