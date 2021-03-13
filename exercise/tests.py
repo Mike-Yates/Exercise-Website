65dcc79ce6dd609d114c5a6c64fec0b98e18ac98
Nathaniel Gonzalez <neg2mhs@virginia.edu>
2021-03-12 21:19:29 -0500

    update tests.py
    fixed test case

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
-class randomTestCase(TestCase):
-    def sample_test_case(self):
-        response = self.client.get(url)
+class RandomTestCase(TestCase):
+    def test_case(self):
+        response = 'https://exercisegamification.herokuapp.com/'
         error404 = False
         if(response.status_code == 404):
             error404 = True
